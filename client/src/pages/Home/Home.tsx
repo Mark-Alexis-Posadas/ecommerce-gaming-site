d68a9f4027acad9d10894a51dc208a2beeddc140
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2025-07-13 19:19:01 +0800

    added banner in homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
+const images = ["/banner/banner-one.avif", "/banner/banner-two.avif"];
+
 export default function Home() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <section>banner</section>
+      <section
+        className="min-h-[600px] bg-cover bg-center bg-no-repeat transition-all duration-1000 ease-in-out"
+        style={{ backgroundImage: `url(${images[index]})` }}
+      ></section>
+
       <section>featured</section>
       <section>categories</section>
     </>
